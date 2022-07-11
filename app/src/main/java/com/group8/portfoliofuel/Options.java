19fd1fe9eed4bccdb7e966beffea75cf6a542976
ammara yamin <ayamin2@uh.edu>
2022-07-10 19:46:41 -0500

    assignment3

--- app/src/main/java/com/group8/portfoliofuel/Options.java
+++ app/src/main/java/com/group8/portfoliofuel/Options.java
@@ -2,13 +2,27 @@ package com.group8.portfoliofuel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Options extends AppCompatActivity {
-
+    private Button quotehistory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        quotehistory = findViewById(R.id.btnRegister4);
+
+        quotehistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Options.this, quotehistory.class));
+            }
+        });
     }
+
 }
--- app/src/main/java/com/group8/portfoliofuel/quotehistory.java
+++ app/src/main/java/com/group8/portfoliofuel/quotehistory.java
@@ -1,13 +1,25 @@
 package com.group8.portfoliofuel;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class quotehistory extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotehistory);
+
+
+
     }
 }
+
