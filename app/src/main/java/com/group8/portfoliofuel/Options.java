e51b8d18e157fe85c4756480b4604ceeb53c6272
Areesha56567 <100553962+Areesha56567@users.noreply.github.com>
2022-07-10 22:48:34 -0500

    Update Options.java
    Updated

--- app/src/main/java/com/group8/portfoliofuel/Options.java
+++ app/src/main/java/com/group8/portfoliofuel/Options.java
@@ -10,17 +10,55 @@ import android.widget.TextView;
 
 public class Options extends AppCompatActivity {
     private Button quotehistory;
+    private Button profileManag;
+    private Button fuelForm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
+        profileManag = findViewById(R.id.btnRegister2);
+
+        fuelForm = findViewById(R.id.btnRegister3);
+
         quotehistory = findViewById(R.id.btnRegister4);
 
+        Button logout = findViewById(R.id.btnlogout);
+
+        // will take client to profile management
+        profileManag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Options.this, profileManag.class));
+            }
+        });
+
+        // will take client to fuel form
+        fuelForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Options.this, fuelForm.class));
+            }
+        });
+
+
+        // will take client to quote history
         quotehistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Options.this, quotehistory.class));
+            }
+        });
+
+
+        //Log out will take user to main screen
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logOut = new Intent(Options.this, MainActivity.class);
+
+                startActivity(logOut);
             }
         });
     }
