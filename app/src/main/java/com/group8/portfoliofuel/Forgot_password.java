93770cee1c451868e70e1ab911b30e3da0b13391
EChauras <ektachaurasia02@gmail.com>
2022-08-03 17:51:37 -0700

    Demo

--- app/src/main/java/com/group8/portfoliofuel/Forgot_password.java
+++ app/src/main/java/com/group8/portfoliofuel/Forgot_password.java
@@ -9,6 +9,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +20,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Forgot_password extends AppCompatActivity {
 
-    private EditText mEmail, moldPass, mnewPass;
+    private EditText mEmail;
     private Button resetBtn;
-
+    private ProgressBar progressBar;
     private FirebaseAuth mAuth;
 
     @Override
@@ -30,46 +31,26 @@ public class Forgot_password extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password);
 
         mEmail = findViewById(R.id.Email);
-        moldPass = findViewById(R.id.oldpass);
-        mnewPass = findViewById(R.id.newpass);
         resetBtn = findViewById(R.id.resetpass);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         mAuth = FirebaseAuth.getInstance();
 
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                reset();
+                //resetPassword();
             }
         });
-    }
-        private void reset() {
-            String Email2 = mEmail.getText().toString();
-            String oldpass = moldPass.getText().toString();
-            String newpass = mnewPass.getText().toString();
 
-            if (!Email2.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(Email2).matches()) {
-                if (!oldpass.isEmpty()) {
-                    if (!newpass.isEmpty()) {
-                        mAuth.createUserWithEmailAndPassword(Email2, newpass)
-                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        Toast.makeText(Forgot_password.this, "Reset Successfully !!", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(Forgot_password.this, SignIn.class));
-                                        finish();
-                                    }
-                                });
-                    } else {
-                        mnewPass.setError("Empty Fields are not allowed");
-                    }
-                } else {
-                    moldPass.setError("Empty Fields are not allowed");
-                }
-            }else if (Email2.isEmpty()){
-                mEmail.setError("Empty Fields are not allowed");
-            }else{
-                mEmail.setError("Empty Fields are not allowed");
-            }
-        }
+    }
+    //private void resetPassword {
+     //   String email = mEmail.getText().toString().trim();
+
+       // if (email.isEmpty()){
+         //   mEmail.setError("Email is required");
+          //  mEmail.requestFocus();
+           // return;
+
+
 }
